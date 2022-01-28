600f116286124962228db59f22aa43f52cf9a7e2
alvinashiatey <alvinashiatey@gmail.com>
2022-01-28 11:14:52 -0500

    src/

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ const { resolve } = require("path");
 export default defineConfig({
   resolve: {
     alias: {
-      "@": resolve(__dirname, "/src"),
+      "@": resolve(__dirname, "src/"),
     },
   },
   plugins: [
