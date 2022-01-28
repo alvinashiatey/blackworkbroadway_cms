98d8f2c57022f921c79c148e83c81f3e9a017241
alvinashiatey <alvinashiatey@gmail.com>
2022-01-28 08:55:03 -0500

    change @ to ..

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/blackworkbroadway_cms/",
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
