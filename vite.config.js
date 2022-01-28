da5a3837673cfd94ea2076ae68ce85728879f0fa
alvinashiatey <alvinashiatey@gmail.com>
2022-01-28 09:21:05 -0500

    use resolve in plugin

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,20 @@ import resolve from "@rollup/plugin-node-resolve";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     "@": resolve(__dirname, "./src"),
+  //   },
+  // },
 
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    resolve({
+      alias: {
+        "@": resolve(__dirname, "./src"),
+      },
+    }),
+  ],
   server: {
     port: 9090,
   },
