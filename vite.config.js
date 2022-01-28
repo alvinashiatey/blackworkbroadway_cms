0349a7ecf2255c3e704f2afb1102453cd37c8da0
alvinashiatey <alvinashiatey@gmail.com>
2022-01-28 09:24:13 -0500

    use resolve in plugin

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,20 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import resolve from "@rollup/plugin-node-resolve";
+const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // resolve: {
-  //   alias: {
-  //     "@": resolve(__dirname, "./src"),
-  //   },
-  // },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 
   plugins: [
     vue(),
     resolve({
-      alias: {
-        "@": resolve(__dirname, "./src"),
-      },
+      extensions: [".js", ".ts"],
     }),
   ],
   server: {
