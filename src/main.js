59d0d5ce3349f64f8cbb82836fee86e621861e0b
alvinashiatey <alvinashiatey@gmail.com>
2022-02-26 14:00:53 -0500

    serve setup for the api access

--- src/main.js
+++ src/main.js
@@ -4,10 +4,14 @@ import App from "@/App.vue";
 import axios from "axios";
 import "./scss/reset.scss";
 
-axios.defaults.baseURL =
-  import.meta.env.VITE_API_URL + import.meta.env.VITE_API_VERSION ||
-  "https://localhost:3000/v1";
-console.log(import.meta.env.API_URL);
+if (import.meta.env.PROD) {
+  axios.defaults.baseURL =
+    import.meta.env.VITE_API_URL_PROD + import.meta.env.VITE_API_VERSION
+} else {
+  axios.defaults.baseURL =
+    import.meta.env.VITE_API_URL + import.meta.env.VITE_API_VERSION ||
+    "https://localhost:3000/v1";
+}
 axios.defaults.headers.common["Authorization"] = `${localStorage.getItem(
   "token"
 )}`;
