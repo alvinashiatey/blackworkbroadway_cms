db7330126460e50d8800a243ae62da3630ba6655
alvinashiatey <alvinashiatey@gmail.com>
2022-03-05 13:13:14 -0500

    publish script

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -44,6 +44,7 @@ export default {
                 ModalUploadVue,
                 ImageCapsuleVue,
         },
+
         setup() {
                 playStore.actions.fetchPlays();
                 const router = useRouter();
